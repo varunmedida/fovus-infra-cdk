fc35f100e50dd6bb2c7d287eeb7cf1dcefe6b2a9
Varun Medida <vmedida1@student.gsu.edu>
2024-05-08 14:27:15 -0400

    Initial commit

---
+++ test/fovus-infra-cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as FovusInfraCdk from '../lib/fovus-infra-cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/fovus-infra-cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new FovusInfraCdk.FovusInfraCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
